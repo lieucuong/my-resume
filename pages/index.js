ab072a140e1dc809750b01ffef5fac79bc1c0510
cuongchung <lieucuong2505@rowboatsoftware.com>
2021-12-07 18:29:00 +0700

    update github action

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/home.module.css'
+import styles from '../styles/Home.module.css'
 import Introduction from '../components/introduction'
 import Experience from '../components/experience'
 import Project from '../components/project'
